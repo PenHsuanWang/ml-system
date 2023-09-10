c7c2a3a3df58e3876ba78ed4c85f40cccb5a87c6
PenHsuanWang <willy118116@gmail.com>
2023-09-10 18:53:38 +0800

    refector the mlflow client

--- src/model_ops_manager/mlflow_agent/client.py
+++ src/model_ops_manager/mlflow_agent/client.py
@@ -4,6 +4,10 @@ This module is defined for the client of mlflow_agent
 from abc import ABC, abstractmethod
 
 import mlflow
+import mlflow.entities.model_registry as mlflow_model_registry
+from mlflow.entities.model_registry.registered_model import RegisteredModel
+from mlflow.exceptions import MlflowException
+
 
 # import src.model_ops_manager.mlflow_agent.utils as mlflow_utils
 
@@ -13,16 +17,25 @@ import mlflow
 
 
 class MLFlowClient(ABC):
+    """
+    The base class of mlflow client to interact with mlflow server
+    Singleton design of client to make all the concrete class share the same mlflow client
+    for specific application or use case need particular implement of mlflow client can inherit this class
+    """
 
     mlflow_client = None
 
     @classmethod
     def init_mlflow_client(cls):
+        """
+        initialize the mlflow client, make sure the tracking uri is set before initialize the client
+        :return:
+        """
+        if not mlflow.tracking.get_tracking_uri():
+            raise ValueError("The tracking uri is not set, please set the tracking uri first")
         if not cls.mlflow_client:
             cls.mlflow_client = mlflow.tracking.MlflowClient()
-
-    def __init__(self):
-        pass
+            print(f"mlflow client initialized, the tracking uri is {mlflow.tracking.get_tracking_uri()}")
 
 
 class MLFlowClientModelAgent(MLFlowClient):
@@ -32,8 +45,40 @@ class MLFlowClientModelAgent(MLFlowClient):
     implement several universal methods of model check logic and model uri composition logic
     """
 
+    # ======================================================================
+    # Simple unit function to check the specific model is registered or not
+    # ======================================================================
+
     @classmethod
-    def is_model_name_exist(cls, model_name: str) -> bool:
+    def is_model_version_registered(cls, model_name: str, model_version: int) -> bool:
+        """
+        A simple method to check the model version is registered or not
+        provide model_name and model_version to check the model version is registered
+        :param model_name:
+        :param model_version:
+        :return:
+        """
+        try:
+            desired_model_version = cls.mlflow_client.get_model_version(model_name, model_version)
+            if isinstance(desired_model_version, mlflow_model_registry.ModelVersion):
+                return True
+            else:
+                return False
+        except MlflowException as mle:
+            print(
+                f"fetching the model {model_name} with version: {model_version} from mlflow server failed, the error is {mle}")
+            print(f"current client tracking uri is {mlflow.tracking.get_tracking_uri()}, please check the tracking uri")
+            return False
+        except Exception as e:
+            print("Unexpected error:", e)
+            return False
+
+    # ======================================================================
+    # Simple unit function to fetch model information
+    # ======================================================================
+
+    @classmethod
+    def get_mlflow_registered_model(cls, model_name: str) -> RegisteredModel:
         """
         check the model name is valid
         :param model_name:
@@ -42,36 +87,28 @@ class MLFlowClientModelAgent(MLFlowClient):
         if not isinstance(model_name, str):
             raise TypeError("The model name should be string")
 
-        # model name exist in the registry
-        model_registry = cls.mlflow_client.get_registered_model(
-            name=model_name
-        )
-        if not model_registry:
+        try:
+            model_registry = cls.mlflow_client.get_registered_model(
+                name=model_name
+            )
+            return model_registry
+        except MlflowException as mle:
+            print(f"fetching the model {model_name} from mlflow server failed, the error is {mle}")
+            print(f"current client tracking uri is {mlflow.tracking.get_tracking_uri()}, please check the tracking uri")
             raise ValueError(f"The model name: {model_name} is not registered")
 
-        # to implement more validation logic if needed
-
-        return True
-
     @classmethod
-    def get_target_model_version(cls, model_name: str, model_version=None, model_stage=None) -> int:
+    def get_model_latest_version(cls, model_name: str, model_stage=None) -> int:
         """
-        Provide the model name, model version or model stage to get the model version
+        Provide the model name, and optional desired stage(s) to get the model latest version for provided stage(s)
         return the corresponding model version to access model artifact.
         The model_name is required, and provide model version or model stage to get the latest version with corresponding stage,
         if the model version is provided, the model_stage will be ignored.
         if the model version is not provided, the model stage is required.
-        :param model_name:
-        :param model_version:
-        :param model_stage:
+        :param model_name: the model name should be a string
+        :param model_stage: desired stage(s) to get the model latest version for provided stage(s), should be in category of ["None", "Staging", "Production", "Archived"]
         :return:
         """
-
-        if not cls.is_model_name_exist(model_name):
-            raise ValueError(f"The model name: {model_name} is not registered")
-
-        if model_version:
-            return model_version
 
         if model_stage:
             # to check the model stage is in category
@@ -81,45 +118,70 @@ class MLFlowClientModelAgent(MLFlowClient):
             else:
                 raise ValueError(
                     f"The model stage should be in {category}, but the provided model stage is {model_stage}")
+            try:
+                model_version = cls.mlflow_client.get_latest_versions(
+                    name=model_name,
+                    stages=[model_stage]
+                )[0].version
+                return model_version
+            except MlflowException as mle:
+                print(
+                    f"fetching the model {model_name} with stage: {model_stage} from mlflow server failed, the error is {mle}")
+                print(
+                    f"current client tracking uri is {mlflow.tracking.get_tracking_uri()}, please check the tracking uri")
+                raise ValueError(f"The model name: {model_name} with stage: {model_stage} is not registered")
 
-            model_version = cls.mlflow_client.get_latest_versions(
-                name=model_name,
-                stages=[model_stage]
-            )[0].get("version")
+        else:
+            try:
+                model_version = cls.mlflow_client.get_latest_versions(
+                    name=model_name,
+                )[0].version
+                return model_version
+            except MlflowException as mle:
+                print(f"fetching the model {model_name} from mlflow server failed, the error is {mle}")
+                print(
+                    f"current client tracking uri is {mlflow.tracking.get_tracking_uri()}, please check the tracking uri")
+                raise ValueError(f"The model name: {model_name} is not registered")
 
-        return model_version
-
-    @classmethod
-    def get_model_latest_version(cls, model_name: str) -> int:
-        """
-        Simply provide the model name to get the latest version of the model
-        :param model_name:
-        :return:
-        """
-        if not cls.is_model_name_exist(model_name):
-            raise ValueError(f"The model name: {model_name} is not registered")
-
-        model_version = cls.mlflow_client.get_latest_versions(
-            name=model_name,
-        )[0].get("version")
-        return model_version
+    # ===============================================================================
+    # function to composite the referenced resource to fetch model from mlflow server
+    # ===============================================================================
 
     @classmethod
     def compose_model_uri(cls, model_name: str, model_version=None, model_stage=None) -> str:
         """
         providing the arbitrary model name, model version and model stage composition.
         To get the specific model uri to access the model artifact by model_name and exact version number
+        model_name is necessary, and provide optionally model_version or model_stage to compose model uri,
+        if only provide model_name, the latest model version will be fetched
+        if provide model_version together, will check model with provided version is registered
+        if provide model_stage together, will get the latest version with corresponding stage.
+        if provide both model_version and model_stage, model_version will be used. Cross-check the desired model stage match with the model version or provide warning message
         :param model_name: the model name should be a string
         :param model_version: the model version should be an integer
         :param model_stage: the model stage should be in category of ["None", "Staging", "Production", "Archived"]
         :return: models:/{model_name}/{model_version}
         """
-        model_version = cls.get_target_model_version(model_name, model_version, model_stage)
-        model_uri = f"models:/{model_name}/{model_version}"
-        return model_uri
 
+        if isinstance(model_name, str):
+            pass
+        else:
+            raise TypeError("The model name should be string")
 
-class MLFlowClientModelLoader(MLFlowClientModelAgent):
+        if isinstance(model_version, int):
+            if cls.is_model_version_registered(model_name, model_version):
+                pass
+            else:
+                print(f"The model name: {model_name} with version: {model_version} is not registered")
+                raise ValueError(f"The model name: {model_name} with version: {model_version} is not registered")
+
+        elif model_version is None and isinstance(model_stage, str):
+            model_version = cls.get_model_latest_version(model_name, model_stage)
+
+        else:
+            model_version = cls.get_model_latest_version(model_name)
+
+        return f"models:/{model_name}/{model_version}"
 
     @classmethod
     def get_download_model_uri(cls, model_name: str, model_version=None, model_stage=None) -> str:
@@ -135,13 +197,25 @@ class MLFlowClientModelLoader(MLFlowClientModelAgent):
         :return: the artifact server uri for downloading the model artifact
         """
 
-        model_version = cls.get_target_model_version(model_name, model_version, model_stage)
+        if model_stage:
+            stage_latest_version = cls.get_model_latest_version(model_name, model_stage)
+            if model_version:
+                if model_version == stage_latest_version:
+                    pass
+                else:
+                    print(
+                        f"The model version: {model_version} is not match with the latest version with corresponding stage: {stage_latest_version}")
+                    raise ValueError(
+                        f"The model version: {model_version} is not match with the latest version with corresponding stage: {stage_latest_version}")
 
         download_model_uri = cls.mlflow_client.get_model_version_download_uri(
             name=model_name,
             version=model_version
         )
         return download_model_uri
+
+
+class MLFlowClientModelLoader(MLFlowClientModelAgent):
 
     @classmethod
     def load_model_as_pyfunc(cls, *args, **kwargs) -> mlflow.pyfunc:
@@ -205,8 +279,6 @@ class MLFlowClientModelLoader(MLFlowClientModelAgent):
                 model = mlflow.pyfunc.load_model(model_uri)
                 return model
 
-
-
     @classmethod
     def get_all_version_registered_model(cls, model_name: str):
         """
@@ -220,16 +292,13 @@ class MLFlowClientModelLoader(MLFlowClientModelAgent):
         cls.mlflow_client.get_registered_model()
 
 
-
-
 if __name__ == "__main__":
     mlflow.set_tracking_uri("http://localhost:5011")
 
-    # client = MLFlowClient()
+    mlflow_client = MLFlowClientModelAgent()
+    mlflow_client.init_mlflow_client()
 
-    # MLFlowClientModelLoader.init_mlflow_client()
-    # MLFlowClientModelLoader.get_download_model_uri(model_name="Pytorch_Model", model_stage="Production")
+    model_uri = mlflow_client.compose_model_uri("Pytorch_Model", 1)
+    print(model_uri)
 
-    # model_uri = "models:/Pytorch_Model/Production"
-    # model = mlflow.pytorch.load_model(model_uri)
-    # breakpoint()
+    print(mlflow_client.get_model_latest_version("Pytorch_Model", "Production"))
